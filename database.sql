6cd7e6c4dc2fae82dcba0cf714304e2bd5b2c5f7
Mohan <mohan1166.be20@chitkarauniversity.edu.in>
2023-06-23 20:10:55 +0530

    added favicon | implemented routes /profile/:sr_no , /add_student

--- database.sql
+++ database.sql
@@ -8,25 +8,29 @@ show tables;
 
 create table batch(
 	sr_no int auto_increment primary key,
-    first_name varchar(50),
+    roll_no int not null,
+    first_name varchar(50) not null,
     last_name varchar(50),
-    age int
+    age int,
+    email varchar(255),
+    phone_no varchar(20),
+    blood_grp varchar(5),
+    father_name varchar(50),
+    mother_name varchar(50)
 );
 
 desc batch;
 
-insert into batch(first_name,last_name,age) values ("Alan","White",21);
-insert into batch(first_name,last_name,age) values ("John","Wick",19);
-insert into batch(first_name,last_name,age) values ("Jake","Parkar",22);
-insert into batch(first_name,last_name,age) values ("Blaze","Claymore",21);
-insert into batch(first_name,last_name,age) values ("Naruto","Uzumaki",18);
-insert into batch(first_name,last_name,age) values ("Sasuke","Uchiha",19);
+
+insert into batch(roll_no,first_name,last_name,age,email,phone_no,blood_grp,father_name,mother_name) values (100,"Alan","White",21,"alan.white@gmail.com","+917265937462","B+","Pyro White","Zen White");
+insert into batch(roll_no,first_name,last_name,age,email,phone_no,blood_grp,father_name,mother_name) values (101,"Blaze","Claymore",18,"blaze.claymore@gmail.com","+918947355427","A+","Alaxander Claymore","Alisha Claymore");
+insert into batch(roll_no,first_name,last_name,age,email,phone_no,blood_grp,father_name,mother_name) values (102,"Vayu","",22,"vayu@gmail.com","+917728364459","B-","Varun","Nisha");
+insert into batch(roll_no,first_name,last_name,age,email,phone_no,blood_grp,father_name,mother_name) values (103,"John","Smith",23,"john.smith@gmail.com","+917834456172","O+","David Smith","Emily Smith");
+insert into batch(roll_no,first_name,last_name,age,email,phone_no,blood_grp,father_name,mother_name) values (104,"Andrew","Anderson",20,"andrew.anderson@gmail.com","+912744639207","AB+","Daniel Anderson","Jessica Anderson");
+insert into batch(roll_no,first_name,last_name,age,email,phone_no,blood_grp,father_name,mother_name) values (105,"James","",21,"James@gmail.com","","B+","Benjamin","Emma");
+
+create view home_view as select sr_no,roll_no,first_name,last_name from batch;
+
+select * from home_view;
 
 select * from batch;
-
-
-
-
-
-
-
--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const mysql=require('mysql');
 
 const connection=mysql.createConnection({
     host: 'localhost',
-    user: 'hostname',
-    password: 'password',
+    user: 'ekansh',
+    password: 'ekansh@329',
     database: 'student'
 });
 
@@ -18,13 +18,13 @@ connection.connect((err)=>{
 })
 
 app.set("view engine","ejs");
-
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
-    const sql='select * from batch';
+    const sql='select * from home_view';
     connection.query(sql,(err,result)=>{
         if(err){
-            console.error('error executing the query',err);
             return;
         }
         res.render('index',{data:result});
@@ -35,12 +35,39 @@ app.get('/profile/:sr_no',(req,res)=>{
     const sql=`select * from batch where sr_no=${req.params.sr_no}`;
     connection.query(sql,(err,result)=>{
         if(err){
-            console.error('error executing the query',err);
             return;
         }
         res.render('profile',{data:result});
     });
 })
+
+app.get('/add_student',(req,res)=>{
+    res.render('add');
+})
+
+app.post('/add_student', (req, res) => {
+    const rollNo = req.body.roll_no;
+    const firstName = req.body.first_name;
+    const lastName = req.body.last_name;
+    const age = req.body.age;
+    const email = req.body.email;
+    const phoneNo = req.body.phone_no;
+    const bloodGroup = req.body.blood_grp;
+    const fatherName = req.body.father_name;
+    const motherName = req.body.mother_name;
+  
+    const sql = `INSERT INTO batch (roll_no, first_name, last_name, age, email, phone_no, blood_grp, father_name, mother_name)
+                 VALUES (${rollNo}, '${firstName}', '${lastName}', ${age}, '${email}', '${phoneNo}', '${bloodGroup}', '${fatherName}', '${motherName}')`;
+  
+    connection.query(sql, (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      res.redirect('/');
+    });
+  });
+  
 
 app.listen(3000,()=>{
     console.log('server started...');
